107675ab8bac874ab61f4c4f36b4d890816c42e5
Dave <126113143+Vin-Dave@users.noreply.github.com>
2023-10-24 22:08:56 +0200

    update exercise

--- project-01/src/App.js
+++ project-01/src/App.js
@@ -1,25 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const messages = [
+	"Learn React ⚛️",
+	"Apply for jobs 💼",
+	"Invest your new income 🤑",
+];
+
+export default function App() {
+	const _URL = "https://type.fit/api/quotes";
+	const [step, setStep] = useState(1);
+	const [data, setData] = useState([]);
+
+	useEffect(() => {
+		fetch(_URL)
+			.then((response) => response.json())
+			.then((data) => {
+				const temp = data.slice(0, 5);
+				setData(temp);
+			});
+	}, []);
+
+	const handleClickPrev = () => {
+		if (step > 0) setStep(step - 1);
+	};
+	const handleClickNext = () => {
+		if (step < 2) setStep(step + 1);
+	};
+
+	return (
+		<>
+			<ItemLi data={data} />
+			<div className='steps'>
+				<div className='numbers'>
+					<div className={step >= 0 ? "active" : ""}>1</div>
+					<div className={step >= 1 ? "active" : ""}>2</div>
+					<div className={step >= 2 ? "active" : ""}>3</div>
+				</div>
+
+				<p className='message'>{messages[step]}</p>
+
+				<div className='buttons'>
+					<button
+						onClick={handleClickPrev}
+						style={{ backgroundColor: "#7950f0", color: "#fff" }}>
+						Prev
+					</button>
+					<button
+						onClick={handleClickNext}
+						style={{ backgroundColor: "#7950f0", color: "#fff" }}>
+						Next
+					</button>
+				</div>
+			</div>
+		</>
+	);
 }
 
-export default App;
+const ItemLi = ({ data }) => {
+	return (
+		<ul>
+			{data.map((element) => (
+				<li key={element.text}>
+					<h2>{element.author}</h2>
+					<span>{element.text}</span>
+				</li>
+			))}
+		</ul>
+	);
+
+	console.log(data);
+};
