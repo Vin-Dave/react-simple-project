19a18d2180972798e0d46c5b27e01435dc243896
Dave <126113143+Vin-Dave@users.noreply.github.com>
2023-10-30 21:44:17 +0100

    create newTast

--- project-05/src/App.js
+++ project-05/src/App.js
@@ -4,6 +4,7 @@ import Form from "./components/Form";
 import Logo from "./components/Logo";
 import PackingList from "./components/PackingList";
 import Stats from "./components/Stats";
+import Appt from "./newTast/Appt";
 
 const initialItems = [
 	{ id: 1, description: "Passports", quantity: 2, packed: true },
