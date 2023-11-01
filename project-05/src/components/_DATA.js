eb8b9918ef5b743ffa1914c9cf918d0f4e3f42b7
Dave <126113143+Vin-Dave@users.noreply.github.com>
2023-11-01 23:08:41 +0100

    create function handlePackedTask

--- project-05/src/components/_DATA.js
+++ project-05/src/components/_DATA.js
@@ -1,6 +1,6 @@
 export const initialItems = [
-  { id: 1, description: "Passports", quantity: 2, packed: true },
-  { id: 2, description: "Socks", quantity: 12, packed: false },
-  { id: 3, description: "Passports2", quantity: 112, packed: true },
-  { id: 4, description: "Socks1", quantity: 122, packed: false },
+	{ id: 1, description: "Passports", quantity: 2, packed: false },
+	{ id: 2, description: "Socks", quantity: 12, packed: false },
+	{ id: 3, description: "Passports2", quantity: 112, packed: true },
+	{ id: 4, description: "Socks1", quantity: 122, packed: false },
 ];
