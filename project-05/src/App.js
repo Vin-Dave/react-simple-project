ed7cc26110105516b8a1096c0d883737b885bf71
Vin-Dave <vin.dave.9702@gmail.com>
2023-11-02 17:01:54 +0100

    update Stats.jsx

--- project-05/src/App.js
+++ project-05/src/App.js
@@ -8,39 +8,39 @@ import Appt from "./newTast/Appt";
 import { initialItems } from "./components/_DATA";
 
 function App() {
-	const [data, setData] = useState(initialItems);
+  const [data, setData] = useState(initialItems);
 
-	const handleAddTask = (task) => {
-		setData((item) => (item = [...data, task]));
-	};
+  const handleAddTask = (task) => {
+    setData((item) => (item = [...data, task]));
+  };
 
-	const handleDeleteTask = (id) => {
-		setData((item) =>
-			item.filter((item) => {
-				return item.id !== id;
-			})
-		);
-	};
+  const handleDeleteTask = (id) => {
+    setData((item) =>
+      item.filter((item) => {
+        return item.id !== id;
+      })
+    );
+  };
 
-	function handlePackedTask(id) {
-		setData((prevData) =>
-			prevData.map((element) =>
-				element.id === id ? { ...element, packed: !element.packed } : element
-			)
-		);
-	}
-	return (
-		<div className='app'>
-			<Logo />
-			<Form addTask={handleAddTask} data={data} />
-			<PackingList
-				deleteTask={handleDeleteTask}
-				data={data}
-				packedTask={handlePackedTask}
-			/>
-			<Stats />
-		</div>
-	);
+  function handlePackedTask(id) {
+    setData((prevData) =>
+      prevData.map((element) =>
+        element.id === id ? { ...element, packed: !element.packed } : element
+      )
+    );
+  }
+  return (
+    <div className="app">
+      <Logo />
+      <Form addTask={handleAddTask} data={data} />
+      <PackingList
+        deleteTask={handleDeleteTask}
+        data={data}
+        packedTask={handlePackedTask}
+      />
+      <Stats data={data} />
+    </div>
+  );
 }
 
 export default App;
