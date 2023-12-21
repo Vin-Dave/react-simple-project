279b284323a7fb8ab8ca6838a37bc34e7f55c67a
Vin-Dave <vin.dave.9702@gmail.com>
2023-12-21 11:59:59 +0100

    update

--- project-07/api-to-do/src/App.jsx
+++ project-07/api-to-do/src/App.jsx
@@ -4,45 +4,54 @@ import { Forms } from "./components/Forms/Forms";
 import { ListTasks } from "./components/ListTasks/ListTasks";
 import { Popup } from "./components/Popup/Popup";
 function App() {
-	const [tasks, setTasks] = useState([]);
-	const [isPopupOpen, setIsPopupOpen] = useState(false);
-	const addNewTask = (todo) => {
-		setTasks([...tasks, todo]);
-	};
+  const [tasks, setTasks] = useState([]);
+  const [isPopupOpen, setIsPopupOpen] = useState(false);
+  const [currentTask, setCurrentTask] = useState(null);
 
-	const editTask = (id) => {
-		const taskToEdit = tasks.find((task) => task.id === id);
+  const addNewTask = (todo) => {
+    setTasks([...tasks, todo]);
+  };
 
-		setIsPopupOpen(true);
-	};
-	const removeTask = (id) => {
-		setTasks((prev) => prev.filter((task) => task.id !== id));
-	};
+  const editTask = (id) => {
+    const taskToEdit = tasks.find((task) => task.id === id);
+    setCurrentTask(taskToEdit);
+    setIsPopupOpen(true);
+  };
+  console.log(currentTask);
+  const removeTask = (id) => {
+    setTasks((prev) => prev.filter((task) => task.id !== id));
+  };
 
-	const doneTask = (id) => {
-		setTasks((prev) =>
-			prev.map((task) => {
-				if (task.id !== id) {
-					return task;
-				} else {
-					return { ...task, done: true };
-				}
-			})
-		);
-	};
+  const doneTask = (id) => {
+    setTasks((prev) =>
+      prev.map((task) => {
+        if (task.id !== id) {
+          return task;
+        } else {
+          return { ...task, done: true };
+        }
+      })
+    );
+  };
 
-	return (
-		<div className='App'>
-			{isPopupOpen && <Popup />}
-			<Forms addNewTask={addNewTask} />
-			<ListTasks
-				tasks={tasks}
-				removeTask={removeTask}
-				doneTask={doneTask}
-				editTask={editTask}
-			/>
-		</div>
-	);
+  return (
+    <div className="App">
+      {isPopupOpen ? (
+        <Popup
+          setIsPopupOpen={setIsPopupOpen}
+          currentTask={currentTask}
+          setTasks={setTasks}
+        />
+      ) : null}
+      <Forms addNewTask={addNewTask} />
+      <ListTasks
+        tasks={tasks}
+        removeTask={removeTask}
+        doneTask={doneTask}
+        editTask={editTask}
+      />
+    </div>
+  );
 }
 
 export default App;
--- project-07/api-to-do/src/components/Popup/Popup.jsx
+++ project-07/api-to-do/src/components/Popup/Popup.jsx
@@ -1,19 +1,58 @@
+import { useEffect, useState } from "react";
+
 import styles from "./Popup.module.css";
 
-export const Popup = ({ task, onClose }) => {
-	return (
-		<div className={styles.appContainer}>
-			<div className={styles.popupOverlay}>
-				<div className={styles.popup}>
-					<h2>Edit Task</h2>
-					<label>Title:</label>
-					<input type='text' />
-					<label>Description:</label>
-					<textarea />
-					<button className={styles.save}>Save</button>
-					<button className={styles.cancel}>Cancel</button>
-				</div>
-			</div>
-		</div>
-	);
+export const Popup = ({ currentTask, setTasks, setIsPopupOpen }) => {
+  const [editedTask, setEditedTask] = useState({ ...currentTask });
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setEditedTask((prevTask) => ({ ...prevTask, [name]: value }));
+  };
+
+  useEffect(() => {
+    setEditedTask({ ...currentTask });
+  }, [currentTask]);
+
+  const handleSaveButton = () => {
+    setTasks((prevTasks) =>
+      prevTasks.map((task) =>
+        task.id === editedTask.id ? { ...editedTask } : task
+      )
+    );
+
+    setIsPopupOpen(false);
+  };
+
+  return (
+    <div className={styles.appContainer}>
+      <div className={styles.popupOverlay}>
+        <div className={styles.popup}>
+          <h2>Edit Task</h2>
+          <label>Title:</label>
+          <input
+            type="text"
+            name="title"
+            value={editedTask.title}
+            onChange={handleInputChange}
+          />
+          <label>Description:</label>
+          <textarea
+            name="description"
+            value={editedTask.description}
+            onChange={handleInputChange}
+          />
+          <button onClick={handleSaveButton} className={styles.save}>
+            Save
+          </button>
+          <button
+            onClick={() => setIsPopupOpen(false)}
+            className={styles.cancel}
+          >
+            Cancel
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 };
