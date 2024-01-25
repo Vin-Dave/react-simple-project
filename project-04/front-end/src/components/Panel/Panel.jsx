c1f936563a84c9dd81b6ae91bd3963e3db5a88e1
Vin-Dave <vin.dave.9702@gmail.com>
2024-01-25 20:50:42 +0100

    init new task

---
+++ project-04/front-end/src/components/Panel/Panel.jsx
@@ -0,0 +1,115 @@
+import { useState, useEffect, useMemo } from "react";
+import { List } from "../List/List";
+import { Form } from "../Form/Form";
+import { ErrorMessage } from "../ErrorMessage/ErrorMessage";
+import { FilterButton } from "../FilterButton/FilterButton";
+import { Info } from "../Info/Info";
+import styles from "./Panel.module.css";
+import { getCategoryInfo } from "../../utils/getCategoryInfo";
+
+const url = "http://localhost:3000/words";
+
+export function Panel() {
+    const [data, setData] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
+    const [error, setError] = useState(null);
+    const [selectedCategory, setSelectedCategory] = useState(null);
+
+    useEffect(() => {
+        let isCanceled = false;
+        const params = selectedCategory ? `?category=${selectedCategory}` : "";
+        fetch(`${url}${params}`)
+            .then((res) => res.json())
+            .then((res) => {
+                if (!isCanceled) {
+                    setData(res);
+                    setIsLoading(false);
+                }
+            });
+
+        return () => {
+            isCanceled = true;
+        };
+    }, [selectedCategory]);
+
+    const categoryInfo = useMemo(
+        () => getCategoryInfo(selectedCategory),
+        [selectedCategory]
+    );
+
+    function handleFormSubmit(formData) {
+        fetch(url, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(formData),
+        })
+            .then((res) => res.json())
+            .then((res) => {
+                if (!selectedCategory || selectedCategory === res.category) {
+                    setData((prevData) => [...prevData, res]);
+                }
+            });
+    }
+
+    function handleDeleteItem(id) {
+        fetch(`${url}/${id}`, {
+            method: "DELETE",
+        })
+            .then((res) => {
+                if (res.ok) {
+                    setData((prevData) =>
+                        prevData.filter((item) => item.id !== id)
+                    );
+                } else {
+                    throw new Error("Błąd podczas usuwania!");
+                }
+            })
+            .catch((e) => {
+                setError(e.message);
+                setTimeout(() => {
+                    setError(null);
+                }, 3000);
+            });
+    }
+
+    function handleFilterClick(category) {
+        setSelectedCategory(category);
+    }
+
+    if (isLoading) {
+        return <p>Ładowanie</p>;
+    }
+
+    return (
+        <>
+            {error && <ErrorMessage>{error}</ErrorMessage>}
+            <section className={styles.section}>
+                <Info>{categoryInfo}</Info>
+                <Form onFormSubmit={handleFormSubmit} />
+                <div className={styles.filters}>
+                    <FilterButton
+                        active={selectedCategory === null}
+                        onClick={() => handleFilterClick(null)}
+                    >
+                        Wszystkie
+                    </FilterButton>
+                    <FilterButton
+                        active={selectedCategory === "noun"}
+                        onClick={() => handleFilterClick("noun")}
+                    >
+                        Rzeczowniki
+                    </FilterButton>
+                    <FilterButton
+                        active={selectedCategory === "verb"}
+                        onClick={() => handleFilterClick("verb")}
+                    >
+                        Czasowniki
+                    </FilterButton>
+                </div>
+                <List data={data} onDeleteItem={handleDeleteItem} />
+            </section>
+        </>
+    );
+}
